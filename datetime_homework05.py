5ee8f70ac20d41bd59754debd2e8607f7324c71d
Mardon1207 <mardonsultonov8@gmail.com>
2023-05-26 09:14:04 +0500

    5

--- datetime_homework05.py
+++ datetime_homework05.py
@@ -1,2 +1,8 @@
 #Task 5:
 #Write a Python program that prompts the user to enter a specific date in the format "dd-mm-yyyy" and determines the day of the week on which that date falls.
+import datetime
+d=input()
+if int(d[6:10])%4==0:
+    print(True)
+else:
+    print(False)
