97c75830d1a19c7a84992375220db993b6b28598
Mardon1207 <mardonsultonov8@gmail.com>
2023-05-29 09:14:18 +0500

    6

--- datetime_homework06.py
+++ datetime_homework06.py
@@ -1,2 +1,8 @@
 #Task 6:
 #Write a Python program that accepts a date in the format "dd-mm-yyyy" and checks if it is a leap year. Display an appropriate message indicating whether it is a leap year or not.
+import datetime
+d=input()
+if int(d[6:10])%4==0:
+    print(True)
+else:
+    print(False)
