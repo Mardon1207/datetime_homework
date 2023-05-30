1f3fcd1f0c003242fc6f8eb629bed9e6cd193b75
Mardon1207 <mardonsultonov8@gmail.com>
2023-05-31 00:41:02 +0500

    9

--- datetime_homework09.py
+++ datetime_homework09.py
@@ -1,2 +1,8 @@
 #Task 9:
 #Write a Python program that calculates the difference between two given dates. Prompt the user to enter two dates in the format "dd-mm-yyyy" and display the number of days between them.
+import datetime
+s=input()
+now=datetime.datetime.strptime(s, '%d-%m-%Y')
+a=input()
+age=datetime.datetime.strptime(a, '%d-%m-%Y')
+print(now-age)
