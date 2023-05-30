7068a0c2e25e0e4f05f9ccdedbcc451d16164d95
Mardon1207 <mardonsultonov8@gmail.com>
2023-05-31 00:34:49 +0500

    8

--- datetime_homework08.py
+++ datetime_homework08.py
@@ -1,2 +1,6 @@
 #Task 8:
 #Write a Python program that subtracts 10 hours from the current time and displays the resulting time.
+import datetime
+now=datetime.datetime.now()
+a=datetime.timedelta(hours=10)
+print(now-a)
